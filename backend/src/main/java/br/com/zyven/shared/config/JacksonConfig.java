1c56da9292d81aaaf4a8c05e4331adcf8052b737
Torres <arthur.torres@sonner.com.br>
2026-09-21 00:38:57 -0300

    fix: disponibilizar mapper JSON para financeiro

---
+++ backend/src/main/java/br/com/zyven/shared/config/JacksonConfig.java
@@ -0,0 +1,14 @@
+package br.com.zyven.shared.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
